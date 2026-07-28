ccbc1d68fa777306825e239341d81657b308cd98
Kamigad <camilo971939@gmail.com>
2026-07-28 15:08:21 -0500

    creacion del componente Listado de Productos con Angular

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { ProductoLista } from "./producto-lista/producto-lista";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [ProductoLista]
 })
 export class App {
   protected readonly title = signal('frontend');
